f3f5d882fa9916284b5236950d5ea8d8b87a2cc2
Osiris487 <46914556+Osiris487@users.noreply.github.com>
2019-01-31 07:52:58 +0500

    registration

---
+++ registration.php
@@ -0,0 +1,86 @@
+<html>
+<head>
+<link rel="icon" type="image/x-icon" href="favicon.ico">
+<meta charset="windows-1251">
+<link href="styles.css" rel="stylesheet" type="text/css" />
+<title>Sport by Ilya Morozov
+</title>
+<head>
+<body>
+<div class="gmenu">
+    <ul>
+        <li id="gmenus"><a href="index.html">Main</a></li>
+        <li><a href="Sport.html">Sport</a></li>
+        <li><a href="London2012.html">London2012</a></li>
+        <li><a href="Words.html">Words</a></li>
+        <li><a href="Author.html">Author</a></li>
+</div>
+<div class="osnova">
+  <br>
+    	<a href="index.html"><img src=logo.png height="100" width="300"></a>                   
+        <div class="Content"><br><br><br><br><br><br><br><br><br><br><br><br>		
+                            
+							
+							<form action="verification.php" method="POST">
+
+<table>
+<tr>
+<td>Логин<font color="red">*</font>:</td>
+<td><input type="text" size="20" name="login"></td>
+</tr>
+<tr>
+<td>Пароль<font color="red">*</font>:</td>
+<td><input type="password" size="20" maxlength="20" name="password"></td>
+</tr>
+<tr>
+<td>Подтверждения пароля<font color="red">*</font>:</td>
+<td><input type="password" size="20" maxlength="20" name="password2"></td>
+</tr>
+<tr>
+<td>E-mail<font color="red">*</font>:</td>
+<td><input type="text" size="20" name="email"></td>
+</tr>
+<tr>
+<td>Имя:</td>
+<td><input type="text" size="20" name="name"></td>
+</tr>
+<tr>
+<td>Фамилия:</td>
+<td><input type="text" size="20" name="lastname"></td>
+</tr>
+<tr>
+<td></td>
+<td colspan="2"><input type="submit" value="Зарегистроваться" name="submit">
+</td>
+</tr>
+</table>
+</form>
+
+<br>Поля со значком <font color="red">*</font> обязательны для заполнения
+							
+							
+							
+					
+			<br><br><br><br><br><br><br><br><br><br><br><br>				
+            <hr size=3 color="#ff0000" align=center>
+			<a href="#"><img src=strelka.png height="50" width="100" align="right"></a><br><br><br><br> 			
+        </div>				
+        <div class="podmenu">                      
+            <ul class="podmenus"><h3>Menu</h3>
+			<strong>
+				<li><a href="Index.php">--Enter the site--</a></li>
+                <li><a href="Swimming.html">Swimming</a></li>
+                <li><a href="Bodybuilding.html">Bodybuilding</a></li>
+                <li><a href="Run.html">Run</a></li></strong>
+            </ul><br><br><br>                        
+            <ul class="podmenus"><h3>Look more:</h3>
+			<strong>
+				<li><a href="http://www.tvoytrening.ru/" target="_blank">Bodybuilding</a></li>
+				<li><a href="http://www.sovsport.ru/" target="_blank">Sport News</a></li></strong><br><br>
+		</ul>				          
+</div><br>
+ <Font align=justify size=2 face=Arial >Morozov I.S. EVT-16bzu  
+ </Font>
+</div>
+</body>
+</html>
