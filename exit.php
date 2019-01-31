e8b23221cef63f75232acf25d45cba5e41723142
Osiris487 <46914556+Osiris487@users.noreply.github.com>
2019-01-31 07:57:04 +0500

    registration update

--- exit.php
+++ exit.php
@@ -1,8 +1,8 @@
 <?php include_once("bd.php"); ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+
 <html>
 <head>
-<title>БЕСПЛАТНЫЕ уроки для веб програмиста</title>
+<title></title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">	
 </head>
 <body>
